f6f41d6a7229d1ace26f1f5729b5443fe05da364
sawmon71293 <sawmon71293@gmail.com>
2023-05-06 18:13:41 +0630

    Fixed eslint errors'

--- src/__tests__/FishProfiles.test.js
+++ src/__tests__/FishProfiles.test.js
@@ -1,43 +1,43 @@
 import { render, screen } from '@testing-library/react';
-import FishProfiles from '../components/FishProfiles/FishProfiles';
 import { useSelector } from 'react-redux';
 import { MemoryRouter, useLocation } from 'react-router-dom';
+import FishProfiles from '../components/FishProfiles/FishProfiles';
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('react-redux');
 jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'), // Preserve other exported modules
-    useLocation: jest.fn(),
+  ...jest.requireActual('react-router-dom'), // Preserve other exported modules
+  useLocation: jest.fn(),
 }));
 describe('FishProfiles component', () => {
-    it('renders the fish profiles correctly', () => {
-        const fishProfiles = [
-            // Define a smaller set of fish profiles for testing
-            {
-                id: '1',
-                speciesName: 'Fish Species 1',
-                imageUrl: 'fish1.jpg',
-            },
-            {
-                id: '2',
-                speciesName: 'Fish Species 2',
-                imageUrl: 'fish2.jpg',
-            },
-        ];
+  it('renders the fish profiles correctly', () => {
+    const fishProfiles = [
+      // Define a smaller set of fish profiles for testing
+      {
+        id: '1',
+        speciesName: 'Fish Species 1',
+        imageUrl: 'fish1.jpg',
+      },
+      {
+        id: '2',
+        speciesName: 'Fish Species 2',
+        imageUrl: 'fish2.jpg',
+      },
+    ];
 
-        useSelector.mockReturnValue({ fishProfiles });
-        useLocation.mockReturnValue({});
+    useSelector.mockReturnValue({ fishProfiles });
+    useLocation.mockReturnValue({});
 
-        const { getByText, getByAltText } = render(
-            <MemoryRouter>
-                <FishProfiles />
-            </MemoryRouter>
-        );
+    const { getByText } = render(
+      <MemoryRouter>
+        <FishProfiles />
+      </MemoryRouter>,
+    );
 
-        // Assertions
-        expect(getByText('Fish Species 1')).toBeInTheDocument();
-        expect(screen.getByText('Fish Species 2')).toBeInTheDocument();
-        expect(screen.getByAltText('Fish Species 1')).toBeInTheDocument();
-        expect(screen.getByAltText('Fish Species 2')).toBeInTheDocument();
-    });
+    // Assertions
+    expect(getByText('Fish Species 1')).toBeInTheDocument();
+    expect(screen.getByText('Fish Species 2')).toBeInTheDocument();
+    expect(screen.getByAltText('Fish Species 1')).toBeInTheDocument();
+    expect(screen.getByAltText('Fish Species 2')).toBeInTheDocument();
+  });
 });
--- src/__tests__/FishSlice.test.js
+++ src/__tests__/FishSlice.test.js
@@ -6,42 +6,39 @@ const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 describe('fishSlice', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should fetch fish profiles and add them to the state', async () => {
+    const mockProfiles = [
+      {
+        id: '1234',
+        speciesName: 'Test Species',
+        imageUrl: 'https://example.com/image.jpg',
+        region: 'Test Region',
+        availability: 'Test Availability',
+        taste: 'Test Taste',
+        source: 'Test Source',
+        texture: 'Test Texture',
+        healthBenefits: 'Test Health Benefits',
+      },
+    ];
+
+    // Mock the API call
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockProfiles),
     });
 
-    it('should fetch fish profiles and add them to the state', async () => {
+    const store = mockStore({ fishProfiles: [], loading: false });
 
-        const mockProfiles = [
-            {
-                id: '1234',
-                speciesName: 'Test Species',
-                imageUrl: 'https://example.com/image.jpg',
-                region: 'Test Region',
-                availability: 'Test Availability',
-                taste: 'Test Taste',
-                source: 'Test Source',
-                texture: 'Test Texture',
-                healthBenefits: 'Test Health Benefits',
-            },
-        ];
+    await store.dispatch(getProfiles());
 
-        // Mock the API call
-        jest.spyOn(global, 'fetch').mockResolvedValue({
-            json: jest.fn().mockResolvedValue(mockProfiles),
-        });
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual('fish/pending');
+    expect(actions[1].type).toEqual('fish/rejected');
 
-        const store = mockStore({ fishProfiles: [], loading: false });
-
-        await store.dispatch(getProfiles());
-
-        const actions = store.getActions();
-        expect(actions[0].type).toEqual('fish/pending');
-        expect(actions[1].type).toEqual('fish/rejected');
-
-
-
-        // Restore the original fetch function
-        global.fetch.mockRestore();
-    });
+    // Restore the original fetch function
+    global.fetch.mockRestore();
+  });
 });
